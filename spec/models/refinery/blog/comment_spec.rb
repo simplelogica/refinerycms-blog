9d29aeeec66b042581097f6a01ddd8c5eda78800
David J. Brenes <david.brenes@simplelogica.net>
2013-02-15 11:47:15 +0100

    Allowing http connection without vcr cassetes
    Most tests were failing as no http request was allowed outside the VCR environments

--- spec/models/refinery/blog/comment_spec.rb
+++ spec/models/refinery/blog/comment_spec.rb
@@ -4,6 +4,7 @@ require 'vcr'
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/fixtures/vcr/'
   c.hook_into :webmock # or :fakeweb
+  c.allow_http_connections_when_no_cassette = true
 end
 
 module Refinery
